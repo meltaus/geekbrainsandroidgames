f506f71992dc5af8433c6d4124fd9a0ead2695e3
Артем Горбачев <meltaus@yandex.ru>
2018-10-22 21:56:20 +0700

    Отрисовка фона

--- core/src/ru/gorbachev/StarGame.java
+++ core/src/ru/gorbachev/StarGame.java
@@ -4,16 +4,22 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	static Texture backtexture;
+	static Sprite backSprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("spaceshipspr.png");
+		backtexture = new Texture(Gdx.files.internal("background.png"));
+		backSprite = new Sprite(backtexture);
 	}
 
 	@Override
@@ -21,7 +27,10 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		backSprite.setSize(1024, 1024);
+		backSprite.setPosition(0,0f);
+		backSprite.draw(batch);
+		batch.draw(img, 150, 150);
 		batch.end();
 	}
 	
