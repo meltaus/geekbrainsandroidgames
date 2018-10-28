d0b5838408a4f2fb65f5eff894c6aef328ce5400
Артем Горбачев <meltaus@yandex.ru>
2018-10-28 17:32:19 +0700

    Наследование от Game. Возвраение объектра MenuScreen

--- core/src/ru/gorbachev/Start2DGame.java
+++ core/src/ru/gorbachev/Start2DGame.java
@@ -1,4 +1,12 @@
 package ru.gorbachev;
 
-public class Start2DGame {
+import com.badlogic.gdx.Game;
+
+import ru.gorbachev.screen.MenuScreen;
+
+public class Start2DGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new MenuScreen());
+    }
 }
