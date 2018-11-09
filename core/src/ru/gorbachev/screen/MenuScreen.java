a9b5a6f80231a500370c2254f05a598d630b518a
Артем Горбачев <meltaus@yandex.ru>
2018-11-09 22:54:15 +0700

    Revert "Увеличение кнопок по нажатию и уменьшение после отжатия."
    This reverts commit 8490d945ac9ef871d571e51af0546e670c3abed2.

--- core/src/ru/gorbachev/screen/MenuScreen.java
+++ core/src/ru/gorbachev/screen/MenuScreen.java
@@ -26,8 +26,6 @@ public class MenuScreen extends Base2DScreen {
     private Sprite startButtonSprite;
     private Sprite exitButtonSprite;
 
-    private boolean statusPressButton;
-
     public MenuScreen (Start2DGame start2DGame) {
         this.start2DGame = start2DGame;
     }
@@ -46,8 +44,6 @@ public class MenuScreen extends Base2DScreen {
         backgroudRegion.setPosition(-1000, -1000);
         startButtonSprite.setPosition(-BUTTON_WIDTH/2,0);
         exitButtonSprite.setPosition(-BUTTON_WIDTH/2,-BUTTON_HEIGHT);
-
-        statusPressButton = false;
     }
 
     @Override
@@ -69,34 +65,19 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
         if(Gdx.input.justTouched()) {
-            statusPressButton = true;
             // обработка касания по кнопке Start
             if((touch.x>=startButtonSprite.getX()) && touch.x<= (startButtonSprite.getX()+startButtonSprite.getWidth()) && (touch.y>=startButtonSprite.getY()) && touch.y<=(startButtonSprite.getY()+startButtonSprite.getHeight()) ){
-                startButtonSprite.setScale(1.2f);
+                start2DGame.setScreen(new GameScreen());
             }
             // обработка касания по кнопке Exit
             else if((touch.x>=exitButtonSprite.getX()) && touch.x<= (exitButtonSprite.getX()+exitButtonSprite.getWidth()) && (touch.y>=exitButtonSprite.getY()) && touch.y<=(exitButtonSprite.getY()+exitButtonSprite.getHeight()) ){
-                exitButtonSprite.setScale(1.2f);
+                Gdx.app.exit(); // выход из приложения
             }
         }
         return false;
     }
 
-    @Override
-    public boolean touchUp(Vector2 touch, int pointer) {
-        if(statusPressButton) {
-            statusPressButton = false;
-            // обработка касания по кнопке Start
-            if((touch.x>=startButtonSprite.getX()) && touch.x<= (startButtonSprite.getX()+startButtonSprite.getWidth()) && (touch.y>=startButtonSprite.getY()) && touch.y<=(startButtonSprite.getY()+startButtonSprite.getHeight()) ){
-                startButtonSprite.setScale(1f);
-                start2DGame.setScreen(new GameScreen()); // Переход к игре
-            }
-            // обработка касания по кнопке Exit
-            else if((touch.x>=exitButtonSprite.getX()) && touch.x<= (exitButtonSprite.getX()+exitButtonSprite.getWidth()) && (touch.y>=exitButtonSprite.getY()) && touch.y<=(exitButtonSprite.getY()+exitButtonSprite.getHeight()) ){
-                exitButtonSprite.setScale(1f);
-                Gdx.app.exit(); // выход из приложения
-            }
-        }
-        return false;
+    private void showMenu() {
+
     }
 }
