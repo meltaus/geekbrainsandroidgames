a1bb91a456a14a605874c4c602a5a372d72bc9dc
Артем Горбачев <meltaus@yandex.ru>
2018-11-10 10:42:56 +0700

    Отрисовка фона

--- core/src/ru/gorbachev/screen/GameScreen.java
+++ core/src/ru/gorbachev/screen/GameScreen.java
@@ -2,16 +2,19 @@ package ru.gorbachev.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.gorbachev.base.Base2DScreen;
+import ru.gorbachev.object.Background;
 
 public class GameScreen extends Base2DScreen {
 
-
-
     @Override
     public void show() {
+        super.show();
+        background = new Background(new Sprite(backgroundAtlas.findRegion("deepspace2")),
+                screenWeight, screenHeight);
     }
 
     @Override
@@ -19,6 +22,7 @@ public class GameScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+        background.render(batch);
         batch.end();
 
     }
