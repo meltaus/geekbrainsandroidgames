8c6e26596283793fa755fa76da86b6a541f38e98
Артем Горбачев <meltaus@yandex.ru>
2018-11-12 10:29:54 +0700

    Реализация движения коробля

--- core/src/ru/gorbachev/base/StarShip.java
+++ core/src/ru/gorbachev/base/StarShip.java
@@ -13,56 +13,35 @@ public class StarShip {
     private Vector2 newPosition;
     private float deltaV;
 
+    private Vector2 pos;
+    private Vector2 v;
+    private Vector2 buf;
+
+    private float shipWidth;
+    private float shipHeight;
+
     protected void initShip() {
-        float shipWidth = width *resize_factor;
-        float shipHeight = height*resize_factor;
-        position.x = position.x - shipWidth/2;
-        position.y = position.y - shipHeight/2;
-        if (position.x <= -width) {
-            float tmp = -position.x - width;
-            position.x = position.x + tmp;
-        }
-        if (position.x >= width) {
-            float tmp = position.x - width;
-            position.x = position.x - tmp;
-        }
-        if ((position.y - shipHeight/2) <= -height) {
-            float tmp = -position.y - height;
-            position.y = position.y + tmp;
-        }
-        if (position.y + shipHeight/2 >= height) {
-            float tmp = position.y - height;
-            position.y = position.y - tmp;
-        }
-        shipTexture.setSize(shipWidth, shipHeight);
-        shipTexture.setPosition(position.x, position.y);
-        newPosition = position;
+        shipWidth = width *resize_factor;
+        shipHeight = height*resize_factor;
+        buf = new Vector2();
+        newPosition = new Vector2();
+        v = new Vector2();
+        pos = new Vector2(position.x, position.y);
     }
 
     public void render(SpriteBatch batch) {
-        shipTexture.draw(batch);
-    }
-
-    private void setSubPosition() {
-        float x = 0,y = 0;
-        if (position.x < newPosition.x) {
-            x = 5 * deltaV;
+        buf.set(newPosition);
+        if (buf.sub(pos).len() > v.len()) {
+            pos.add(v);
+        } else {
+            pos.set(newPosition);
         }
-        if (position.x > newPosition.x) {
-            x = -5 * deltaV;
-        }
-        if (position.y < newPosition.y) {
-            y = 5 * deltaV;
-        }
-        if (position.y > newPosition.y) {
-            y = -5 * deltaV;
-        }
-        Vector2 tmp = position;
-        tmp.sub(x,y);
+        batch.draw(shipTexture, pos.x - (shipWidth/2), pos.y - (shipHeight/2), shipWidth, shipHeight);
     }
 
     public void moveShip(Vector2 newPosition, float deltaV) {
         this.newPosition = newPosition;
         this.deltaV = deltaV;
+        v.set(newPosition.cpy().sub(pos).scl(0.01f));
     }
 }
